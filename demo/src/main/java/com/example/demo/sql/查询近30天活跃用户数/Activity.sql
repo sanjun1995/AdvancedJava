91e59f81d58818ce9057c7d9c272de892ce82851
caozhixin <593721042@qq.com>
2022-06-05 23:11:58 +0800

    坚持到底 sql boy

--- demo/src/main/java/com/example/demo/sql/查询近30天活跃用户数/Activity.sql
+++ demo/src/main/java/com/example/demo/sql/查询近30天活跃用户数/Activity.sql
@@ -59,3 +59,8 @@ select activity_date as day, count(distinct user_id) as active_users
 from Activity
 where datediff('2019-07-27', activity_date) between 0 and 29
 group by activity_date
+
+select activity_date as day, count(distinct user_id) as active_users
+from Activity
+where DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date) > 0
+group by activity_date
