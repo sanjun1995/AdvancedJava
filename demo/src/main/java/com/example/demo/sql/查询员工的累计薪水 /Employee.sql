ee1045f481c015f6d3c8b46243435372952d4673
caozhixin <593721042@qq.com>
2022-06-07 23:26:13 +0800

    坚持到底 sql boy

--- demo/src/main/java/com/example/demo/sql/查询员工的累计薪水 /Employee.sql
+++ demo/src/main/java/com/example/demo/sql/查询员工的累计薪水 /Employee.sql
@@ -74,3 +74,21 @@ from
     employee E3
     on (E3.id = E1.id and E3.month = E1.month - 2)
 order by E1.id asc, E1.month desc;
+
+
+select Id, AccMonth as Month, sum(Salary) as Salary
+from
+    (
+    select a.Id as Id, a.Month as AccMonth, b.Month as Month, b.Salary as Salary
+    from
+    (
+    select Employee.Id as Id, Employee.Month as Month
+    from Employee, (select Id, max(Month) as Month
+    from Employee
+    group by Id) as LastMonth
+    where Employee.Id = LastMonth.Id and Employee.Month != LastMonth.Month) as a
+    join Employee as b
+    on a.Id = b.Id and a.Month - b.Month <= 2 and a.Month - b.Month >= 0
+    ) as acc
+group by Id, AccMonth
+order by Id, Month desc
