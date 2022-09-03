5806ec617a436c2792889283adca2c26f2ae59d3
caozhixin <593721042@qq.com>
2022-09-03 23:13:18 +0800

    坚持到底 贪心算法

--- demo/src/main/java/com/example/demo/algo/greedy/JumpDemo.java
+++ demo/src/main/java/com/example/demo/algo/greedy/JumpDemo.java
@@ -6,16 +6,30 @@ package com.example.demo.algo.greedy;
  */
 public class JumpDemo {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
+        if (nums == null) {
+            return false;
+        }
         int rightmost = 0;
-        for (int i = 0; i < n; i++) {
-            if (i <= rightmost) {
-                rightmost = Math.max(rightmost, i + nums[i]);
-                if (rightmost >= n - 1) {
-                    return true;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            int temp = i + nums[i];
+            rightmost = Math.max(rightmost, temp);
+            if (rightmost >= nums.length - 1) {
+                return true;
             }
         }
         return false;
     }
+//    public boolean canJump(int[] nums) {
+//        int n = nums.length;
+//        int rightmost = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (i <= rightmost) {
+//                rightmost = Math.max(rightmost, i + nums[i]);
+//                if (rightmost >= n - 1) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
