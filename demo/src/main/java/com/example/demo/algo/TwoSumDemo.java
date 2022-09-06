da7a04c1610de6869ba38e94f33fbfa7c7ac6a27
caozhixin <593721042@qq.com>
2022-09-06 22:52:35 +0800

    坚持到底 三数之和

---
+++ demo/src/main/java/com/example/demo/algo/TwoSumDemo.java
@@ -0,0 +1,48 @@
+package com.example.demo.algo;
+
+import java.util.*;
+
+/**
+ * @author caozhixin
+ * @date 2022/9/6 10:41 PM
+ */
+public class TwoSumDemo {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        // 枚举a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不同
+        }
+        return null;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+//    public static int[] twoSum(int[] nums, int target) {
+//        int n = nums.length;
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = i + 1; j < n; ++j) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[] {i, j};
+//                }
+//            }
+//        }
+//        return new int[0];
+//    }
+}
