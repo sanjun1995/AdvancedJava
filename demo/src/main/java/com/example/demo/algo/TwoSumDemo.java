656bdeba6bc75cde86b9981539476a4948cb96f0
caozhixin <593721042@qq.com>
2022-09-13 09:32:39 +0800

    坚持到底 三数之和

--- demo/src/main/java/com/example/demo/algo/TwoSumDemo.java
+++ demo/src/main/java/com/example/demo/algo/TwoSumDemo.java
@@ -1,5 +1,6 @@
 package com.example.demo.algo;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.*;
 
 /**
@@ -16,13 +17,36 @@ public class TwoSumDemo {
         int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
+        // 枚举 a
         for (int first = 0; first < n; ++first) {
             // 需要和上一次枚举的数不相同
             if (first > 0 && nums[first] == nums[first - 1]) {
                 continue;
             }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着b后续的增加
+                // 就不会有满足 a + b + c = 0 并且 b < c 的 c了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
         }
-        return null;
+        return ans;
     }
 
     public static int[] twoSum(int[] nums, int target) {
