923531c0ec4e27900babb2a8b150401659b39c9c
caozhixin <593721042@qq.com>
2022-09-14 22:40:26 +0800

    坚持到底 移动0

---
+++ demo/src/main/java/com/example/demo/algo/ZeroesDemo.java
@@ -0,0 +1,30 @@
+package com.example.demo.algo;
+
+import java.util.Arrays;
+
+/**
+ * @author caozhixin
+ * @date 2022/9/14 10:33 PM
+ */
+public class ZeroesDemo {
+    public static void main(String[] args) {
+        int[] a = new int[]{0, 1, 0, 3, 12};
+        moveZeroes(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // 遍历元素，不为0，即赋值在nums[j]
+                nums[j] = nums[i];
+                // 如果i == j，说明当前位置不用动，否则置为0
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+     }
+}
