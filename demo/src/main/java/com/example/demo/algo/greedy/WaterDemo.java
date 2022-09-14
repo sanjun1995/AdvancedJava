4c505e0f2ef90458f55aa5fc2cdc56823e9887f7
caozhixin <593721042@qq.com>
2022-09-14 23:10:22 +0800

    坚持到底，水容量

--- demo/src/main/java/com/example/demo/algo/greedy/WaterDemo.java
+++ demo/src/main/java/com/example/demo/algo/greedy/WaterDemo.java
@@ -9,19 +9,29 @@ public class WaterDemo {
 
     }
 
-    public int maxArea(int[] height) {
-        // 本质是求面积
-        int l = 0, r = height.length - 1;
-        int ans = 0;
-        while (l < r) {
-            int area = Math.min(height[l], height[r]) * (r - l);
-            ans = Math.max(ans, area);
-            if (height[l] <= height[r]) {
-                ++l;
-            } else {
-                --r;
-            }
+    public int maxArea(int[] a) {
+        int max = 0;
+        for (int i = 0, j = a.length - 1; i < j;) {
+            int minHeight = a[i] < a[j] ? a[i++] : a[j--];
+            // 前面有i++或j--，故需要+1
+            max = Math.max(max, (j - i + 1) * minHeight);
         }
-        return ans;
+        return max;
     }
+
+//    public int maxArea(int[] height) {
+//        // 本质是求面积
+//        int l = 0, r = height.length - 1;
+//        int ans = 0;
+//        while (l < r) {
+//            int area = Math.min(height[l], height[r]) * (r - l);
+//            ans = Math.max(ans, area);
+//            if (height[l] <= height[r]) {
+//                ++l;
+//            } else {
+//                --r;
+//            }
+//        }
+//        return ans;
+//    }
 }
