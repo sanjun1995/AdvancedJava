46c4d693a121ef43a99bb6ce834de97e92968774
caozhixin <593721042@qq.com>
2022-09-14 22:47:01 +0800

    坚持到底 移动0

--- demo/src/main/java/com/example/demo/algo/ZeroesDemo.java
+++ demo/src/main/java/com/example/demo/algo/ZeroesDemo.java
@@ -9,8 +9,25 @@ import java.util.Arrays;
 public class ZeroesDemo {
     public static void main(String[] args) {
         int[] a = new int[]{0, 1, 0, 3, 12};
-        moveZeroes(a);
+        moveZeroe(a);
         System.out.println(Arrays.toString(a));
+    }
+
+    public static void moveZeroe(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 
     public static void moveZeroes(int[] nums) {
