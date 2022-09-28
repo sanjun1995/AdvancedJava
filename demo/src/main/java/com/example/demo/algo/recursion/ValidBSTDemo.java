6351dd97475f8f9e377581545d1ba7abd60cdcd2
caozhixin <593721042@qq.com>
2022-09-28 10:10:30 +0800

    坚持到底，getSimpleName

--- demo/src/main/java/com/example/demo/algo/recursion/ValidBSTDemo.java
+++ demo/src/main/java/com/example/demo/algo/recursion/ValidBSTDemo.java
@@ -2,22 +2,45 @@ package com.example.demo.algo.recursion;
 
 import com.example.demo.algo.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author caozhixin
  * @date 2022/9/25 9:10 PM
  */
 public class ValidBSTDemo {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
+        Deque<TreeNode> stack = new LinkedList<>();
+        double inorder = -Double.MAX_VALUE;
 
-    private boolean isValidBST(TreeNode node, long lower, long upper) {
-        if (node == null) {
-            return true;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个inorde, 说明不是二叉搜索树
+            // 因为二叉搜索树的中序遍历是有序递增的数列
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
         }
-        if (node.val <= lower || node.val >= upper) {
-            return false;
-        }
-        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+        return true;
     }
+//    public boolean isValidBST(TreeNode root) {
+//        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//    }
+//
+//    private boolean isValidBST(TreeNode node, long lower, long upper) {
+//        if (node == null) {
+//            return true;
+//        }
+//        if (node.val <= lower || node.val >= upper) {
+//            return false;
+//        }
+//        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+//    }
 }
