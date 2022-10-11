ba08f3e6542d4ff0f9d6ec38754be062f5095aa8
caozhixin <593721042@qq.com>
2022-10-11 11:18:03 +0800

    坚持到底，getSimpleName

--- demo/src/main/java/com/example/demo/test/LengthDemo.java
+++ demo/src/main/java/com/example/demo/test/LengthDemo.java
@@ -8,11 +8,12 @@ import com.baomidou.mybatisplus.core.toolkit.StringUtils;
  */
 public class LengthDemo {
     public static void main(String[] args) {
-        String batchName = "交易单号:15794191956222987161579145805296488649";
+        String batchName = "交易单号:157941919562229871615791111";
         // batchName最大长度为32
         int maxLength = 32;
         if (StringUtils.isNotBlank(batchName) && batchName.length() > maxLength) {
-            System.out.println(batchName.substring(0, maxLength).substring(0, maxLength - 3) + "...");
+            batchName = batchName.substring(0, maxLength - 3) + "...";
         }
+        System.out.println(batchName);
     }
 }
