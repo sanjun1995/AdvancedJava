745be92732cf55f5851f5e3da8235e0f4042af72
caozhixin <593721042@qq.com>
2023-03-22 16:47:11 +0800

    坚持到底，getSimpleName

---
+++ demo/src/main/java/com/example/demo/io/Demo01.java
@@ -0,0 +1,23 @@
+package com.example.demo.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author caozhixin
+ * @date 2023/3/22 15:39
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/input.txt");
+            int c;
+            while ((c = fis.read()) != -1) {
+                System.out.print((char) c);
+            }
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
