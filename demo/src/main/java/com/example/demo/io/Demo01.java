85c7feb2ff227b280d9be75febcd5c93a91629b1
caozhixin <593721042@qq.com>
2023-03-23 14:11:33 +0800

    坚持到底，getSimpleName

--- demo/src/main/java/com/example/demo/io/Demo01.java
+++ demo/src/main/java/com/example/demo/io/Demo01.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Demo01 {
     public static void main(String[] args) {
         try {
-            FileInputStream fis = new FileInputStream("src/main/resources/input.txt");
+            FileInputStream fis = new FileInputStream("/Users/caozhixin/IdeaProjects/AdvancedJava/demo/src/main/resources/input.txt");
             int c;
             while ((c = fis.read()) != -1) {
                 System.out.print((char) c);
