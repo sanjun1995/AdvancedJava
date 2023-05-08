f22fcc3754f6af89f06151b7dd53d145825331b6
caozhixin <593721042@qq.com>
2023-05-08 14:06:02 +0800

    坚持到底，getSimpleName

--- demo/src/main/java/com/example/demo/agent/MyClassTransformer.java
+++ demo/src/main/java/com/example/demo/agent/MyClassTransformer.java
@@ -7,12 +7,15 @@ import java.io.File;
 import java.lang.instrument.ClassFileTransformer;
 
 import java.security.ProtectionDomain;
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.objectweb.asm.Opcodes.*;
 
 public class MyClassTransformer implements ClassFileTransformer {
+    // basePackages是需要增强的类所在的包的集合
     private final Set<String> basePackages;
+    // 获取该ClassTransformer类的全限定名，将包名中的点号替换为文件路径中的分隔符
     private static final String OWNER = MyClassTransformer.class.getCanonicalName().replace(".", File.separator);
 
     public MyClassTransformer(Set<String> basePackages) {
@@ -21,66 +24,64 @@ public class MyClassTransformer implements ClassFileTransformer {
 
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        // 判断是否需要对该类进行增强，如果不需要直接返回原字节码数据
         if (!needEnhance(className)) {
             return classfileBuffer;
         }
 
         System.out.println("Transforming class: " + className);
 
-        // asm进行字节码增强
+        // 利用ASM对字节码进行增强
         try {
+            // 创建ClassReader对象
             ClassReader cr = new ClassReader(className);
+            // 创建ClassWriter对象
             ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-            ClassVisitor cv = new ClassVisitor(Opcodes.ASM8, cw) {
+            // 创建ClassVisitor对象，对字节码进行访问
+            ClassVisitor cv = new ClassVisitor(Opcodes.ASM5, cw) {
                 @Override
                 public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                    // 对每个方法进行访问，返回MethodVisitor对象进行访问
                     MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
-                    return new MethodVisitor(Opcodes.ASM8, mv) {
-                        private Label startLabel = new Label();
-                        private Label endLabel = new Label();
+                    // 创建MethodVisitor对象，对方法字节码进行访问
+                    return new MethodVisitor(Opcodes.ASM5, mv) {
+                        // 存储try-catch处理器的标签
+                        private final Set<Label> tryCatchBlockHandlers = new HashSet<>();
 
                         @Override
-                        public void visitCode() {
-                            super.visitCode();
-                            Label tryStart = new Label();
-                            Label tryEnd = new Label();
-                            Label catchHandler = new Label();
-                            mv.visitTryCatchBlock(tryStart, tryEnd, catchHandler, "java/lang/Throwable");
-                            mv.visitLabel(tryStart);
+                        public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
+                            // 对visitTryCatchBlock方法进行访问，在访问方法中存储try-catch处理器的标签
+                            tryCatchBlockHandlers.add(handler);
+                            super.visitTryCatchBlock(start, end, handler, type);
                         }
 
                         @Override
-                        public void visitMaxs(int maxStack, int maxLocals) {
-                            mv.visitLabel(endLabel);
-                            mv.visitInsn(RETURN);
-
-                            Label catchHandler = new Label();
-                            mv.visitLabel(catchHandler);
-                            mv.visitVarInsn(ASTORE, 1);
-                            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-                            mv.visitLdcInsn("Exception caught:");
-                            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
-                            mv.visitVarInsn(ALOAD, 1);
-                            mv.visitMethodInsn(INVOKESTATIC, OWNER, "logStackTrace", "(Ljava/lang/Throwable;)V", false);
-                            mv.visitInsn(ATHROW); // throw the caught exception
-
-                            mv.visitTryCatchBlock(startLabel, endLabel, catchHandler, "java/lang/Throwable");
-                            super.visitMaxs(maxStack, maxLocals);
+                        public void visitLineNumber(int line, Label start) {
+                            // 对visitLineNumber方法进行访问，在访问方法中插入方法调用指令
+                            if (tryCatchBlockHandlers.contains(start)) {
+                                mv.visitMethodInsn(INVOKESTATIC, OWNER, "logStackTrace", "(Ljava/lang/Throwable;)V", false);
+                            }
+                            super.visitLineNumber(line, start);
                         }
                     };
                 }
             };
+            // 开始访问ClassReader中的字节码
             cr.accept(cv, ClassReader.EXPAND_FRAMES);
+
+            // 返回增强后的字节码数据
             return cw.toByteArray();
         } catch (Exception e) {
             System.out.println("MyClassTransformer e=" + e);
         }
+        // 出现异常时返回原字节码数据
         return classfileBuffer;
     }
 
     // 定义方法
     public static void logStackTrace(Throwable throwable) {
-        throwable.printStackTrace(System.out);
+        System.out.println("统一打印堆栈：");
+        throwable.printStackTrace();
     }
 
 
--- demo/src/main/java/com/example/demo/agent/Test.java
+++ demo/src/main/java/com/example/demo/agent/Test.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 public class Test {
     public static void main(String[] args) {
         try {
-            System.out.println("hello world!");
             int i = 1 / 0;
         } catch (Exception e) {
             // 由字节码增强来输出
