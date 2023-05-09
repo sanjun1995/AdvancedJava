2626b0b6d0492f96f0f30bdbdd4a8ee51102ea58
caozhixin <593721042@qq.com>
2023-05-09 19:40:53 +0800

    坚持到底，getSimpleName

--- demo/src/main/java/com/example/demo/starter/HelloService.java
+++ demo/src/main/java/com/example/demo/starter/HelloService.java
@@ -2,6 +2,6 @@ package com.example.demo.starter;
 
 public class HelloService {
     public String sayHello(String message) {
-        return "Hello, " + message + "!";
+        return "Hello " + message + "!";
     }
 }
