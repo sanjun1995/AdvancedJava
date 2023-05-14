75abfcfa645cade68da1d022a999f08f12518538
caozhixin <593721042@qq.com>
2023-05-14 21:54:55 +0800

    坚持到底

--- demo/src/main/java/com/example/demo/agent/Test.java
+++ demo/src/main/java/com/example/demo/agent/Test.java
@@ -2,10 +2,6 @@ package com.example.demo.agent;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @author caozhixin
- * @date 2023/5/6 14:31
- */
 @Slf4j
 public class Test {
     public static void main(String[] args) {
