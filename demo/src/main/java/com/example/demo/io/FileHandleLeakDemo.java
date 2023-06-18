27e45812530abf5d920a5899693d8e477f80aa0d
caozhixin <593721042@qq.com>
2023-06-18 20:14:21 +0800

    坚持到底

--- demo/src/main/java/com/example/demo/io/FileHandleLeakDemo.java
+++ demo/src/main/java/com/example/demo/io/FileHandleLeakDemo.java
@@ -7,17 +7,19 @@ import java.io.IOException;
 public class FileHandleLeakDemo {
     public static void main(String[] args) throws IOException {
         int i = 0;
-        while (true) {
+//        while (true) {
             test();
 //            System.gc();
             System.out.println(i++);
-        }
+//        }
     }
 
     public static void test() throws IOException {
         FileOutputStream fos = new FileOutputStream("demo/src/main/resources/input.txt");
         fos.write("Hello, world!".getBytes());
         // 不关闭文件流
+        fos.close();
+        fos.write("Hello, world!".getBytes());
     }
 }
 
