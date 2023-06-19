fd5e664b83f1e2176896d440240713a0703558ba
caozhixin <593721042@qq.com>
2023-06-19 16:58:27 +0800

    坚持到底

--- demo/src/main/java/com/example/demo/io/Demo02.java
+++ demo/src/main/java/com/example/demo/io/Demo02.java
@@ -1,6 +1,7 @@
 package com.example.demo.io;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -11,15 +12,23 @@ import java.io.IOException;
 public class Demo02 {
     public static void main(String[] args) {
         String fileName = "demo/src/main/resources/input.txt";
-        try (FileReader fileReader = new FileReader(fileName);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                // 处理每行文本
-                System.out.println(line);
+        try (FileInputStream inputStream = new FileInputStream(fileName)) {
+            int word;
+            while ((word = inputStream.read()) != -1) {
+                System.out.println((char) word);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileReader fileReader = new FileReader(fileName)) {
+            int word;
+            while ((word = fileReader.read()) != -1) {
+                System.out.println((char) word);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
