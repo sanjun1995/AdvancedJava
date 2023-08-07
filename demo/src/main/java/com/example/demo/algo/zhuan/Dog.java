b375a86d6a7019e9fd685d1943b45c039cc40d07
caozhixin <593721042@qq.com>
2023-08-07 16:45:56 +0800

    坚持到底

---
+++ demo/src/main/java/com/example/demo/algo/zhuan/Dog.java
@@ -0,0 +1,16 @@
+package com.example.demo.algo.zhuan;
+
+import lombok.Data;
+
+/**
+ * @author caozhixin
+ * @date 2023/8/7 16:34
+ */
+@Data
+public class Dog {
+    Integer age;
+
+    public Dog(Integer age) {
+        this.age = age;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/algo/zhuan/ValueDemo.java
@@ -0,0 +1,31 @@
+package com.example.demo.algo.zhuan;
+
+/**
+ * @author caozhixin
+ * @date 2023/8/7 16:29
+ */
+public class ValueDemo {
+    public static void main(String[] args) {
+        Integer a = 10;
+        Integer b = 12;
+        transform(a, b);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+
+        Dog dog1 = new Dog(10);
+        Dog dog2 = new Dog(12);
+        transform2(dog1, dog2);
+        System.out.println("dog1: " + dog1.getAge());
+        System.out.println("dog2: " + dog2.getAge());
+    }
+
+    public static void transform(Integer a, Integer b) {
+        a = a * 2;
+        b = b * 2;
+    }
+
+    public static void transform2(Dog dog1, Dog dog2) {
+        dog1.setAge(dog1.getAge() * 2);
+        dog2.setAge(dog2.getAge() * 2);
+    }
+}
