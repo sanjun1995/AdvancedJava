29c06e6d6c6a39d36dbf9d008dd953c6e43043ea
caozhixin <593721042@qq.com>
2023-08-25 01:05:47 +0800

    坚持到底

---
+++ demo/src/main/java/com/example/demo/algo/zhuan/IntegerDemo.java
@@ -0,0 +1,15 @@
+package com.example.demo.algo.zhuan;
+
+/**
+ * @author caozhixin
+ * @date 2023/8/23 11:00
+ */
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
+    }
+}
--- demo/src/main/java/com/example/demo/algo/zhuan/ValueDemo.java
+++ demo/src/main/java/com/example/demo/algo/zhuan/ValueDemo.java
@@ -5,21 +5,28 @@ package com.example.demo.algo.zhuan;
  * @date 2023/8/7 16:29
  */
 public class ValueDemo {
+//    public static void main(String[] args) {
+//        Integer a = 10;
+//        Integer b = 12;
+//        transform(a, b);
+//        System.out.println("a: " + a);
+//        System.out.println("b: " + b);
+//
+//        Dog dog1 = new Dog(10);
+//        Dog dog2 = new Dog(12);
+//        transform2(dog1, dog2);
+//        System.out.println("dog1: " + dog1.getAge());
+//        System.out.println("dog2: " + dog2.getAge());
+//    }
+
     public static void main(String[] args) {
-        Integer a = 10;
-        Integer b = 12;
-        transform(a, b);
+        int a = 10;
+        int b = 12;
         System.out.println("a: " + a);
         System.out.println("b: " + b);
-
-        Dog dog1 = new Dog(10);
-        Dog dog2 = new Dog(12);
-        transform2(dog1, dog2);
-        System.out.println("dog1: " + dog1.getAge());
-        System.out.println("dog2: " + dog2.getAge());
     }
 
-    public static void transform(Integer a, Integer b) {
+    public static void transform(int a, int b) {
         a = a * 2;
         b = b * 2;
     }
