86417e2e6626397641bc96c5d58c44c13855197a
caozhixin <593721042@qq.com>
2023-08-25 01:17:55 +0800

    坚持到底

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,15 @@
 package com.example.demo;
 
-import com.example.starterdemo.anno.EnableAutoConfigTest;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@EnableAutoConfigTest
-@SpringBootApplication
-public class DemoApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-
-}
+//import com.example.starterdemo.anno.EnableAutoConfigTest;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@EnableAutoConfigTest
+//@SpringBootApplication
+//public class DemoApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+//    }
+//
+//}
--- demo/src/test/java/com/example/demo/DemoTestSpringBootStarterApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoTestSpringBootStarterApplicationTests.java
@@ -1,21 +1,21 @@
 package com.example.demo;
 
-import com.example.starterdemo.demo.ServiceBean;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-/**
- * @author caozhixin
- * @date 2023/5/10 10:18
- */
-@SpringBootTest
-public class DemoTestSpringBootStarterApplicationTests {
-    @Autowired
-    private ServiceBean serviceBean;
-
-    @Test
-    void contextLoads() {
-        System.out.println(serviceBean.sayHello("geek"));
-    }
-}
+//import com.example.starterdemo.demo.ServiceBean;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+///**
+// * @author caozhixin
+// * @date 2023/5/10 10:18
+// */
+//@SpringBootTest
+//public class DemoTestSpringBootStarterApplicationTests {
+//    @Autowired
+//    private ServiceBean serviceBean;
+//
+//    @Test
+//    void contextLoads() {
+//        System.out.println(serviceBean.sayHello("geek"));
+//    }
+//}
