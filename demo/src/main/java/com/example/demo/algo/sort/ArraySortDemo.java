f298cc3bb9a9064caace8970dc5f36fa14166726
caozhixin <593721042@qq.com>
2023-09-04 17:35:12 +0800

    坚持到底

--- demo/src/main/java/com/example/demo/algo/sort/ArraySortDemo.java
+++ demo/src/main/java/com/example/demo/algo/sort/ArraySortDemo.java
@@ -4,12 +4,31 @@ package com.example.demo.algo.sort;
  * @author caozhixin
  * @date 2023/4/5 22:04
  */
-import java.util.Arrays;
 
 public class ArraySortDemo {
     public static void main(String[] args) {
-        int[] arr = {5, 7, 3, 9, 2, 1, 6, 4, 8};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        System.out.println(findKthLargest(arr, 2));
+    }
+
+    static int quickSelect(int[] nums, int l, int r, int k) {
+        if (l == r) return nums[k];
+        int x = nums[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            do i++; while (nums[i] < x);
+            do j--; while (nums[j] > x);
+            if (i < j){
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+        if (k <= j) return quickSelect(nums, l, j, k);
+        else return quickSelect(nums, j + 1, r, k);
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        return quickSelect(nums, 0, n - 1, n - k);
     }
 }
