15492155f13fb07b1c8b05e8965f25c53664c295
caozhixin <593721042@qq.com>
2023-12-05 10:36:56 +0800

    坚持到底

---
+++ demo/src/main/java/com/example/demo/gc/MemoryDemo.java
@@ -0,0 +1,20 @@
+package com.example.demo.gc;
+
+/**
+ * @author caozhixin
+ * @date 2023/12/4 19:48
+ */
+public class MemoryDemo {
+    static int num = 0;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            System.out.println("Child：" + num);
+            num++;
+            System.out.println("Child End：" + num);
+        }).start();
+
+        while (num == 0);
+        System.out.println("Main：" + num);
+    }
+}
